4202e0104eb57b6953a3ddda144115d2b14143d1
Jet Braun <jetbraun@gmail.com>
2024-07-17 13:53:01 -0500

    Updated lemma substitutions

--- text_utils.py
+++ text_utils.py
@@ -12,7 +12,13 @@ WIKITAG_EXPR = re.compile("==+[^=]*?==+|\\n|\\r|{[^}]*?}")
 # Match more than one space
 EXTRASPACE_EXPR = re.compile("  +")
 
-PUNCT_EXPR = re.compile(r"[^a-z0-9|\.]")
+PUNCT_EXPR = re.compile(r"[^a-z0-9|\.|\?]")
+
+PRONOUNS_EXPR = re.compile("[he|she|they|we|it]")
+
+LEMMAS = {
+    "[he|she|they|we|it]'d" : "[he|she|they|we|it] had",
+}
 
 def preprocess_text(text: str, repl: str=" ") -> str:
 
@@ -25,14 +31,19 @@ def preprocess_text(text: str, repl: str=" ") -> str:
 
     text = re.sub(PUNCT_EXPR, repl, text)
 
+    # this is lazy, but so am i
     text = re.sub(" d ", " had ", text)
     text = re.sub(" s ", " is ", text)
     text = re.sub(" ve ", " have ", text)
+    text = re.sub(" m ", " am ", text)
+    text = re.sub(" ll ", " will ", text)
 
     text = re.sub(EXTRASPACE_EXPR, repl, text)
 
     return text
 
-      
-
-
+def load_stopwords(txt_path: str, sep:str=","):
+    with open(txt_path, "r") as f:
+        text = f.read()
+    text = text.strip(" ")
+    return text.split(sep) 

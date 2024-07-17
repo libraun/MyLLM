b845275c0d3380bddb3642a95caf76474e30ac3a
Jet Braun <jetbraun@gmail.com>
2024-07-16 20:15:25 -0500

    Updated decoder forward pass; refactored all methods in TextTensorBuilder to be either class or static methods; changed 'collate' (create_tensors.py) to concatenate a tuple of input documents (instead of requiring it to already be joined; upgraded 'SentenceTransformerEmbeddings' to HuggingFaceEmbeddings; major readability changes to 'create_tensors.py'

--- create_tensors.py
+++ create_tensors.py
@@ -2,12 +2,17 @@ import json
 import sys
 import os
 
+import math
+import itertools
+
 import torch
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
 import text_utils
 
@@ -15,7 +20,12 @@ from text_tensor_builder import TextTensorBuilder
 
 from constants import *
 
-def load_data(path: str="./Topical-Chat/conversations/train.json"):
+import chromadb
+
+
+def load_topicalchat_data(db_instance, wiki_instance,
+              path: str="./Topical-Chat/conversations/train.json",
+              maxcount: int=5000):
     if not os.path.isfile(path):
 
         exit(-1)
@@ -23,145 +33,186 @@ def load_data(path: str="./Topical-Chat/conversations/train.json"):
     with open(path, "r") as f:
 
         json_object = json.load(f)
-    
     conversation_data = list()
     for conversation in json_object.values():
+
+        if count >= maxcount:
+            break
 
         all_message_data = conversation["content"]
 
         agent1_data, agent2_data = list(), list()
         for message_data in all_message_data:
 
-            msg_text = message_data["message"]
-            msg_sentiment = message_data["sentiment"]
+            msg_text = text_utils.preprocess_text(message_data["message"])
 
-            msg_text = text_utils.preprocess_text(msg_text)
+            if message_data["agent"] == "agent_1":
+                if db_instance is not None:
+                    doc = db_instance.similarity_search(msg_text)[0].page_content
+                else:
+                    doc = wiki_instance.run(msg_text)
 
-            msg_agent = message_data["agent"]
-            msg_pair = (msg_text, msg_sentiment)
-
-            if msg_agent == "agent_1":
-                agent1_data.append(msg_pair)
+                doc = text_utils.preprocess_text(doc)
+                agent1_data.append((msg_text, doc))
             else:
-                agent2_data.append(msg_pair)
+                agent2_data.append(msg_text)
+            
+            if count >= maxcount:
+                break
+            else:
+                count = count + 1
+                sys.stdout.write(str(count) + " / " + str(max_data))
+                sys.stdout.flush()
+
+                sys.stdout.write("\r")
+
+        conversation_data += itertools.batched(list(zip(agent1_data,agent2_data)),n=2)
         conversation_data.append([agent1_data, agent2_data])
     return conversation_data
 
 def collate(data_batch):
 
-    global bos_idx, eos_idx, pad_idx
-    global tensor_builder
+    global BOS_IDX, EOS_IDX, BMD_IDX, EMD_IDX, PAD_IDX
     in_batch,out_batch = [],[]
     for (in_item, out_item) in data_batch:
+
+        in_msg_tensor, in_doc_tensor = in_item
+        in_msg_tensor = torch.cat([
+            torch.tensor([BOS_IDX], dtype=torch.long),
+            in_msg_tensor,
+            torch.tensor([EOS_IDX], dtype=torch.long)
+        ], dim=-1)
+        in_doc_tensor = torch.cat([
+            torch.tensor([BMD_IDX], dtype=torch.long),
+            in_doc_tensor,
+            torch.tensor([EMD_IDX], dtype=torch.long)
+        ], dim=-1)
+
         in_batch.append(
-            tensor_builder.embedding(
-                torch.cat([
-                    torch.tensor([bos_idx],dtype=torch.long),
-                    in_item,
-                    torch.tensor([eos_idx],dtype=torch.long)], dim=0
-                )
-            )
+            torch.cat([
+                in_msg_tensor, in_doc_tensor
+            ], dim=-1)
         )
         out_batch.append(
-            tensor_builder.embedding(
-                torch.cat([
-                    torch.tensor([bos_idx],dtype=torch.long),
-                    out_item,
-                    torch.tensor([eos_idx],dtype=torch.long)], dim=0
-                )
-            )
+            torch.cat([
+                torch.tensor([BOS_IDX], dtype=torch.long),
+                out_item,
+                torch.tensor([EOS_IDX], dtype=torch.long)
+            ], dim=-1)
         )
-    in_batch = pad_sequence(in_batch, padding_value=pad_idx)
-    out_batch = pad_sequence(out_batch, padding_value=pad_idx)
-
+    in_batch = pad_sequence(in_batch,padding_value=PAD_IDX)
+    out_batch = pad_sequence(out_batch, padding_value=PAD_IDX)
     return in_batch,out_batch
+
+def load_stopwords(txt_path: str, sep:str=","):
+    with open(txt_path, "r") as f:
+        text = f.read()
+    text = text.strip(" ")
+    return text.split(sep) 
 
 # Query chroma using langchain for similar input documents, 
 # convert text to tensors and save
 
 if __name__ == "__main__":
-    
-    input_output_data = load_data() 
-    
-    query_data = list()
-    for convo in input_output_data:
-        for agent in convo: 
-            for message in agent:
-                query_data.append(message[0])
-                
-    tensor_builder = TextTensorBuilder(EMBED_DIM, query_data)
 
-    tokenizer = tensor_builder.tokenizer
-    en_vocab = tensor_builder.get_vocab()
+    max_data = int(sys.argv[1])
 
-    bos_idx = en_vocab["<bos>"]
-    eos_idx = en_vocab["<eos>"]
-    pad_idx = en_vocab["<pad>"]
+    db_name = DEFAULT_DB_NAME if len(sys.argv) < 3   \
+        else sys.argv[2] 
+    db_path = DEFAULT_DB_PATH if len(sys.argv) < 4   \
+        else sys.argv[3] 
+    
+    wikipedia_instance = None
+    if os.path.isdir(db_path):
+
+        embedding_function = HuggingFaceEmbeddings(
+            model_name="all-MiniLM-L6-v2")
+
+        client = chromadb.PersistentClient(path=db_path)
+        collection = client.get_collection(db_name)
+
+        db_instance = Chroma(
+            client=client, 
+            collection_name="chatbot",
+            embedding_function=embedding_function
+        )
+    else:
+        print("""
+              WARNING: Persistent ChromaDB instance could not be found.
+              Program will use Langchain wikipedia to directly query input documents, 
+              which will be very, very slow.
+
+              In the future, please consider adding preprocessed documents
+              to a persistent chromaDB instance, and loading the sentence embeddings from there.
+              """)
+        wikipedia_instance = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+        
+    
+    all_data,agent1_msgs, agent2_msgs = load_data(
+        db_instance=db_instance, wiki_instance=wikipedia_instance) 
+
+    stopwords = load_stopwords("stopwords.txt")
+
+    agent1_special_tokens = [
+        "<UNK_IDX>", "<PAD_IDX>",
+        "<BOS_IDX>", "<EOS_IDX>", 
+        "<BEGIN_MD_IDX>", "<END_MD_IDX>"
+    ]
+
+    all_vocab_list = list()
+    for (agent1_msg, agent1_md_doc), agent2_msg in all_data:
+        
+        all_vocab_list.append(agent1_msg)
+        all_vocab_list.append(agent1_md_doc)
+
+        all_vocab_list.append(agent2_msg)
+
+    en_vocab = TextTensorBuilder.build_vocab(all_vocab_list, specials=agent1_special_tokens)
+
+    TextTensorBuilder.save_vocab(en_vocab, "all_vocab.pickle")
+
+    BOS_IDX = en_vocab["<BOS_IDX>"]
+    EOS_IDX = en_vocab["<EOS_IDX>"]
+    PAD_IDX = en_vocab["<PAD_IDX>"]
+
+    BMD_IDX = en_vocab["<BEGIN_MD_IDX>"]
+    EMD_IDX = en_vocab["<END_MD_IDX>"]
 
     tensor_data = list()
 
-    data_len_str = str(len(input_output_data))
-    count = 0
-
-    input_text_docs, output_text_docs = list(), list()
-    '''
-    batch(input_output_data)
-    '''
-
     wikipedia_instance = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-    for agent1_data, agent2_data in input_output_data[ : 2500]:
+    for (agent1_msg, agent1_doc), agent2_msg in all_data:
         
-        min_agent_data_length = len(min(agent1_data, agent2_data, key=len))
-
+        in_msg_tensor = TextTensorBuilder.text_to_tensor(agent1_msg)
+        in_md_tensor = TextTensorBuilder.text_to_tensor(agent1_doc)
         
-        for i in range(min_agent_data_length):
-            input_msg = agent1_data[i][0]
-            output_msg = agent2_data[i][0]
-
-            doc = wikipedia_instance.run(input_msg)
-            doc = text_utils.preprocess_text(doc)
-
-            input_msg_ids = tokenizer(input_msg)
-            doc_ids = tokenizer(doc)
-
-            input_doc = input_msg_ids + ["<BEGINDOC>"] + doc_ids + ["<ENDDOC>"]
-
-            agent1_msg_tensor = tensor_builder.convert_text_to_tensor(
-                input_msg, tokenize=False)
-            agent2_msg_tensor = tensor_builder.convert_text_to_tensor(
-                output_msg )
-            
-            full_doc = (agent1_msg_tensor, agent2_msg_tensor)
-            tensor_data.append(full_doc)
-
-            count = count + 1
-
-            sys.stdout.write(str(count) + " / " + data_len_str)
-            sys.stdout.flush()
-
-            sys.stdout.write("\r")
+        out_msg_tensor = TextTensorBuilder.text_to_tensor(agent2_msg)
+        
+        tensor_data.append(((in_msg_tensor, in_md_tensor), out_msg_tensor))
 
 
-    train_data = tensor_data[ : 20000]
-    valid_data = tensor_data[ 20000 : 22000 ]
-    test_data = tensor_data[ 22000: 23000 ]
+    train_end_idx = math.floor(len(tensor_data) * 0.8) 
+    valid_end_idx = math.floor(len(tensor_data) * 0.9)
+
+    train_data = tensor_data[ : train_end_idx]
+    valid_data = tensor_data[ train_end_idx : valid_end_idx ]
+    test_data = tensor_data[ valid_end_idx : ]
 
     train_iter = DataLoader(train_data, 
-                            batch_size=BATCH_SIZE,
-                            shuffle=True,
+                            batch_size=64,
+                            shuffle=False,
                             collate_fn=collate)
     valid_iter = DataLoader(valid_data, 
-                            batch_size=BATCH_SIZE,
-                            shuffle=True,
+                            batch_size=64,
+                            shuffle=False,
                             collate_fn=collate)
     test_iter = DataLoader(test_data, 
-                           batch_size=BATCH_SIZE,
-                           shuffle=True,
+                           batch_size=64,
+                           shuffle=False,
                            collate_fn=collate)
 
     torch.save(train_iter, "./model_tensors/train_tensor.pt")
     torch.save(valid_iter, "./model_tensors/valid_tensor.pt")
-    
     torch.save(test_iter, "./model_tensors/test_tensor.pt")
 
     exit(EXIT_SUCCESS)
--- text_tensor_builder.py
+++ text_tensor_builder.py
@@ -1,5 +1,6 @@
+import pickle
+
 import torch
-import torch.nn as nn
 import torchtext
 
 torchtext.disable_torchtext_deprecation_warning()
@@ -11,46 +12,25 @@ from torchtext.data.utils import get_tokenizer
 
 class TextTensorBuilder:
 
-    def __init__(self, 
-                 embedding_dim: int, 
-                 data: List[str],
-                 tokenizer: str="spacy",
-                 tokenizer_lang: str="en_core_web_sm",
-                 specials: List[str]=["<unk>", "<pad>", "<bos>", "<eos>", "<BEGINDOC>","<ENDDOC>"],
-                 default_token: str="<unk>"):
-        
-        if default_token not in specials:
-            specials.insert(0, default_token)
-
-        self.tokenizer = get_tokenizer(tokenizer,tokenizer_lang)
-        self.en_vocab = self.__build_vocab__(data, specials)
-
-        self.en_vocab.set_default_index(self.en_vocab[default_token])
-
-        self.embedding = nn.Embedding(len(self.en_vocab), embedding_dim,
-                                      padding_idx=self.en_vocab["<pad>"])
+    tokenizer = get_tokenizer("spacy","en_core_web_sm")
     
-    def convert_text_to_tensor(self, 
-                               doc: str | List[str], 
-                               tokenize: bool=True ) -> torch.Tensor: 
-        if tokenize:
-            tokens = self.tokenizer(doc)
-        else:
-            tokens = doc
-        text_tensor = [self.en_vocab[token] for token in tokens]
+    @classmethod
+    def text_to_tensor(self, lang_vocab,
+                       doc: str | List[str], 
+                       tokenize: bool=True ) -> torch.Tensor: 
+        
+        tokens = doc if not tokenize else self.tokenizer(doc)
+        
+        text_tensor = [lang_vocab[token] for token in tokens]
         text_tensor = torch.tensor(text_tensor, dtype=torch.long)
 
         return text_tensor
-    
-    def get_vocab(self):
-        return self.en_vocab
 
-    def __build_vocab__(self,
-                        corpus: List[str],
-                        specials: List[str]):
+    @classmethod
+    def build_vocab(cls,corpus: List[str], specials: List[str]):
         counter = Counter()
         for text in corpus:
-            tokens = self.tokenizer(text)
+            tokens = cls.tokenizer(text)
             counter.update(tokens)
 
         sorted_by_freq_tuples = sorted(counter.items(), 
@@ -61,3 +41,9 @@ class TextTensorBuilder:
         result = vocab(ordered_dict, specials=specials)
 
         return result
+    
+    @staticmethod
+    def save_vocab(lang_vocab, filename: str):
+
+        with open(filename, "wb+") as f:
+            pickle.dump(lang_vocab)

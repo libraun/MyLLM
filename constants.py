a0a23226dd43ab4e08fed9ff84c0a914be71183c
Jet Braun <jetbraun@gmail.com>
2024-07-18 16:15:09 -0500

    Changed constants to be more descriptive and added new constants

--- constants.py
+++ constants.py
@@ -1,31 +1,44 @@
-LOG_PROGRESS = True
+# Exit Codes
+EXIT_FAILURE: int = -1
+EXIT_SUCCESS: int = 0
 
-EXIT_FAILURE = -1
-EXIT_SUCCESS = 0
+# Chroma related constants (building db)
 
-DEFAULT_NUM_PAGES = 50
-DEFAULT_NUM_THREADS = 5 # Set to 1 for no threading
+DEFAULT_NUM_PAGES: int = 50
+DEFAULT_NUM_THREADS: int = 5 # Set to 1 for no threading
 
-DEFAULT_DB_NAME = "chatbot"
-DEFAULT_DB_PATH = "chroma_data"
+DEFAULT_DB_NAME: str = "chatbot"
+DEFAULT_DB_PATH: str = "chroma_data"
 
-USE_THREADING = True
-LOG_TIME_INFO = True
+USE_THREADING: bool = True
+LOG_TIME_INFO: bool = True
 
 TOTAL_NUM_PAGES: int = 5000
+COLLECTION_ROUNDS: int = 500
+BATCH_LEN: int = int(TOTAL_NUM_PAGES / COLLECTION_ROUNDS)
 
-ROUNDS: int = 500
-BATCH_LEN: int = int(TOTAL_NUM_PAGES / ROUNDS)
+LOG_COLLECTION_PROGRESS = True # Whether or not to log number of documents processed by workers
 
-BATCH_SIZE = 256
+# Preprocessing Constants
+TOPICALCHAT_PATH: str = "./Topical-Chat/conversations/train.json"
+
+DEFAULT_MAX_MSGS: int = 50000
+STRIP_INPUT_STOPWORDS: bool = False 
 
 # NN related constants
 
-EMBED_DIM = 512
-HIDDEN_DIM = 64
+DATALOADER_BATCH_SIZE: int = 256
+SHUFFLE_DATALOADERS: bool = False
 
-NUM_EPOCHS = 10
+SPECIAL_TOKENS: list = [
+    "<UNK_IDX>", "<PAD_IDX>",
+    "<BOS_IDX>", "<EOS_IDX>", 
+    "<BEGIN_MD_IDX>", "<END_MD_IDX>"
+]
 
-MAX_DECODER_OUTPUT_LENGTH = 20
+HIDDEN_DIM: int = 64
+NUM_EPOCHS: int = 10
 
-TRAIN_UPDATE_MSG = "Epoch {n}: Train loss={t_loss:.2f} | Eval loss = {e_loss:.2f}"
+MAX_DECODER_OUTPUT_LENGTH: int = 20
+
+TRAIN_UPDATE_MSG: str = "Epoch {n}: Train loss={t_loss:.2f} | Eval loss = {e_loss:.2f}"

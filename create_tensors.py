77aa420dd3ace0b504018d0b5bb4cccfeb0bccd2
Jet Braun <jetbraun@gmail.com>
2024-07-27 13:52:18 -0500

    Updated create tensors to create batches of (input_msg, input_md, output_msg) (see last commit)

--- create_tensors.py
+++ create_tensors.py
@@ -1,40 +1,24 @@
 import json
 import sys
 import os
-
+import pickle
 import argparse
 import itertools
-
 import math
-
 import chromadb
-
 import torch
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
-
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-
 import logging
-
 from typing import List, Tuple
 
 import text_utils as text
-
 from text_tensor_builder import TextTensorBuilder
-
 import constants
-
-def pretty_print(out: str):
-
-    sys.stdout.write(out)
-    sys.stdout.flush()
-
-    sys.stdout.write("\r")
-    
 
 # NOTE: maxcount can be any value greater than 0 (does not need to be less than number of messages in dataset)
 def create_data(query_config: Chroma | WikipediaQueryRun,
@@ -52,15 +36,17 @@ def create_data(query_config: Chroma | WikipediaQueryRun,
 
         convo = [text.preprocess(msg) for msg in convo]
 
-        input_output_document = list(itertools.batched(convo, n=2))
+        input_output_document = list(itertools.pairwise(convo))
+
         if len(input_output_document[-1]) != 2:
             input_output_document = input_output_document[ : -1]
+
         for input_doc, output_doc in input_output_document:
             
             if isinstance(query_config, Chroma):
                 md_doc, score = query_config.similarity_search_with_score(input_doc, k=1)[0]
 
-                md_doc = text.preprocess(md_doc) if score < max_similarity_score else "<NOMD>"
+                md_doc = text.preprocess(md_doc.page_content) if score < max_similarity_score else ""
             else:     
                 md_doc = text.preprocess(query_config.run(input_doc), stopwords=stopwords)
 
@@ -71,51 +57,95 @@ def create_data(query_config: Chroma | WikipediaQueryRun,
 
 def collate(data_batch):
 
-    in_batch, out_batch = zip(*data_batch)
+    in_batch, md_batch, out_batch = zip(*data_batch)
     
     in_batch = pad_sequence(in_batch, padding_value=PAD_IDX)
+    md_batch = pad_sequence(md_batch, padding_value=PAD_IDX)
     out_batch = pad_sequence(out_batch, padding_value=PAD_IDX)
     
-    return in_batch,out_batch
+    return in_batch,md_batch, out_batch
 
 
 parser = argparse.ArgumentParser(
     prog="Create Tensors",
-    description="""
-    This program queries a Chroma instance (or WikipediaQueryAPI) to build pytorch dataloaders. It accepts the following arguments:
+    description="This program queries a Chroma instance (or WikipediaQueryAPI) to build pytorch dataloaders.",
+    usage="""
+        The following arguments are accepted.   
+        --input-path (-i)         REQUIRED STR    The filepath to the (.json) training source.
+        --train-output-path       REQUIRED STR    The filepath to save train DataLoader to (recommended to have a '.pt' extension)
+        --valid-output-path       STR             The filepath to save valid DataLoader to (recommended to have a '.pt' extension)
 
-        --count (-c) INT The max number of messages to process, or None to process all data. (Default None)
-        --batch-size (-b) INT The (unsigned integer) size of each batch for dataloader. (Default 128)
+        --vocab-path (-v)         STR             Path to an existing torchtext.vocab object to use during DataLoader creation.
+    
+        --count (-c)              REQUIRED INT    The max number of messages to process.
+        --batch-size (-b)         REQUIRED INT    The (unsigned integer) size of each batch for dataloader. (Default 64)
         
-        --shuffle (-s) Whether or not to shuffle the dataloaders. (Default False)
-        --drop-last-batch (-d) Whether or not to drop the last (incomplete) batch. (Default False)
+        --shuffle (-s)            FLAG            Create the dataloaders with the default random batch sampler. (Default False)
+        --drop-last-batch (-d)    FLAG            Drop the last (incomplete) batch. (Default False)
 
-        --remove-stopwords (-r) Whether or not to remove stopwords from (queried) documents. (Default False)
-        --use-wikipedia-api (-w) Whether or not to use the Wikipedia API to directly request documents. (Default False)
+        --clean-stopwords         FLAG            Remove stopwords from (queried) documents. (Default False)
+        --no-lemmatize            FLAG            Do not lemmatize input words. (Default True)
 
-        --reverse-encoder-inputs Whether or not to reverse input documents. (Default False)
+        --use-wikipedia-api (-w)  FLAG            Use the Wikipedia API to directly request documents. (Default False)
 
-        --dataset-split-ratio FLOAT How much of the dataset should be reserved for training (between 0.1 and 1)
+        --reverse-encoder-inputs  FLAG            Reverse input documents (not output documents). (Default False)
 
-        --max-similarity-score FLOAT The maximum similarity score for retrieved documents to be considered (values about max_similarity_score will not be considered)
+        --dataset-split-ratio     FLOAT           How much of the dataset should be reserved for training (between 0.1 and 1). (Default 0.8)
+
+        --max-similarity-score    FLOAT           The maximum similarity score for retrieved documents to be considered 
+                (values about max_similarity_score will not be considered). (Default 1.23)
     """,
     epilog="For more information, see the article on PyTorch DataLoaders")
-parser.add_argument("-c", "--count", type=int, default=None)
-parser.add_argument("-b", "--batch-size", type=int, default=64)
-parser.add_argument("-s", "--shuffle", default=False,action="store_true")
-parser.add_argument("-d", "--drop-last-batch", default=False, action="store_true")
-parser.add_argument("-r", "--remove-stopwords", default=False, action="store_true")
-parser.add_argument("-w", "--use-wikipedia-api", default=False, action="store_true")
-parser.add_argument("--reverse-encoder-inputs", default=False, action="store_true")
 
-parser.add_argument("--dataset-split-ratio", type=float, default=0.8)
+parser.add_argument("-i", "--input-path", type=str, required=True,
+                    help="The path to a (existing) .json dataset.")
 
-parser.add_argument("--max-similarity-score", type=float, default=1.23)
+parser.add_argument("--train-output-path", type=str, required=True, 
+                    help="The path (including filename) to store train dataloader.")
+parser.add_argument("--valid-output-path", type=str, required=False, 
+                    help="The path (including filename) to store train dataloader.")
+
+
+parser.add_argument("-v", "--vocab-path", type=str, default=None, required=False, 
+                    help="Supply the path to a preexisting vocab object to use it during training creation.")
+
+parser.add_argument("-c", "--count", type=int, required=True,
+                    help="Count determines the (lower bound) of messages to be processed.")
+parser.add_argument("-b", "--batch-size", type=int, required=True)
+
+parser.add_argument("-s", "--shuffle", default=False, action="store_true", 
+                    help="Enabling this feature results in the DataLoader being sampled from randomly every epoch.")
+parser.add_argument("-d", "--drop-last-batch", default=False, action="store_true",help="""
+                    Enabling this feature results in the last batch of the DataLoader being dropped,
+                    potentially avoiding sequences that are just padding.""")
+parser.add_argument("-r", "--reverse-encoder-inputs", default=False, action="store_true", 
+                    help="Enabling this feature ensures that all encoder inputs are reversed.")
+
+parser.add_argument("--clean-stopwords", default=False, action="store_true", required=False)
+parser.add_argument("--no-lemmatize", default=True, action="store_false", required=False)
+
+parser.add_argument("--use-wikipedia-api", default=False, action="store_true", required=False)
+
+parser.add_argument("--dataset-split-ratio", type=float, default=0.8, required=False)
+
+parser.add_argument("--max-similarity-score", type=float, default=1.23, required=False)
+
 # Query chroma using langchain for similar input documents, 
 # convert text to tensors and save
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+
+    try:
+        args = parser.parse_args()
+    except:
+        exit(constants.EXIT_FAILURE)
+        
+    INPUT_PATH = args.input_path
+    
+    TRAIN_OUTPUT_PATH = args.train_output_path
+    VALID_OUTPUT_PATH = args.valid_output_path
+    
+    VOCAB_PATH = args.vocab_path
 
     MAX_DATA = args.count
     MODEL_BATCH_SIZE = args.batch_size
@@ -123,13 +153,16 @@ if __name__ == "__main__":
     DROP_LAST_BATCH = args.drop_last_batch
 
     USE_WIKIPEDIA = args.use_wikipedia_api
-    REMOVE_STOPWORDS = args.remove_stopwords
+    CLEAN_STOPWORDS = args.clean_stopwords
 
     MAX_SIMILARITY_SCORE = args.max_similarity_score
 
     REVERSE_ENCODER_INPUTS = args.reverse_encoder_inputs
 
     DATASET_SPLIT_RATIO = args.dataset_split_ratio
+
+    assert len(INPUT_PATH) > 0 and len(TRAIN_OUTPUT_PATH) > 0, \
+        "Please provide valid input and A patoutput paths."
 
     assert DATASET_SPLIT_RATIO >= 0.1 and DATASET_SPLIT_RATIO <= 1.0, \
         "For dataset_split_ratio, provide a floating point number greater than 0.1 and <= 1.0"
@@ -162,13 +195,14 @@ if __name__ == "__main__":
         query_config = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(top_k_results=1))
     
     # Stopwords: A list of (very common) strings to remove from the input metadata document.
-    stopwords = None if not REMOVE_STOPWORDS else text.load_tokens_from_text("stopwords.txt")
+    stopwords = None if not CLEAN_STOPWORDS else text.load_tokens_from_text("stopwords.txt")
 
     # Load Topical-Chat dataset from from json
-    if not os.path.isfile(constants.TOPICALCHAT_PATH):
+    if not os.path.isfile(INPUT_PATH):
+        parser.print_usage()
         exit(constants.EXIT_FAILURE)
 
-    with open(constants.TOPICALCHAT_PATH, "r") as f:
+    with open(INPUT_PATH, "r") as f:
         json_object = json.load(f)
 
     # All data is a list containing tuples of the form:
@@ -177,24 +211,43 @@ if __name__ == "__main__":
         query_config, conversations=json_object,  
         max_similarity_score=MAX_SIMILARITY_SCORE,
         stopwords=stopwords, maxcount=MAX_DATA) 
-    
-    vocab_data = list()
+    in_vocab_data, md_vocab_data, out_vocab_data = list(), list(), list()
+    all_vocab_data = list()
     for (in_msg, in_doc), out_msg in all_data:
-        vocab_data.append(in_msg)
-        vocab_data.append(in_doc)
-        vocab_data.append(out_msg)
+        in_vocab_data.append(in_msg)
+        md_vocab_data.append(in_doc)
+        out_vocab_data.append(out_msg)
 
-    # Build and save current vocab for model lookups
-    tensor_builder = TextTensorBuilder(
-        corpus=vocab_data, specials=constants.SPECIAL_TOKENS, 
-        default_index=constants.SPECIAL_TOKENS.index("<UNK>"),
-        min_freq=5,
-        save_filepath="en_vocab.pickle")
-    
-    BOS_IDX, EOS_IDX, PAD_IDX, BMD_IDX, EMD_IDX = tuple(
-        tensor_builder.lang_vocab.lookup_indices([
-            "<BOS>", "<EOS>", "<PAD>", "<BMD>", "<EMD>"
+        all_vocab_data.append(in_msg)
+        all_vocab_data.append(in_doc)
+        all_vocab_data.append(out_msg)
+
+    in_vocab = TextTensorBuilder.build_vocab(
+        corpus=in_vocab_data, specials=["<PAD>", "<UNK>", "<BOS>", "<EOS>"], 
+        default_index=1, min_freq=5,
+        save_filepath="in_vocab.pickle")
+    md_vocab = TextTensorBuilder.build_vocab(
+        corpus=md_vocab_data, specials=["<PAD>", "<UNK>", "<BMD>", "<EMD>"], 
+        default_index=1, min_freq=5,
+        save_filepath="md_vocab.pickle")
+    out_vocab = TextTensorBuilder.build_vocab(
+        corpus=out_vocab_data, specials=["<PAD>", "<UNK>", "<BOS>", "<EOS>"], 
+        default_index=1, min_freq=5,
+        save_filepath="out_vocab.pickle")
+    all_vocab = TextTensorBuilder.build_vocab(
+        corpus=all_vocab_data, specials=["<PAD>", "<UNK>", "<BOS>", "<EOS>", "<BMD>", "<EMD>"], 
+        default_index=1, min_freq=5,
+        save_filepath="all_vocab.pickle")
+
+    BOS_IDX, EOS_IDX, PAD_IDX = tuple(
+        all_vocab.lookup_indices([
+            "<BOS>", "<EOS>", "<PAD>"
         ] ))
+    BMD_IDX, EMD_IDX = tuple(
+        all_vocab.lookup_indices([
+            "<BMD>", "<EMD>"
+        ])
+    )
 
     # Tensorize docs before creating a DataLoader instance
     tensor_data = list()
@@ -202,32 +255,24 @@ if __name__ == "__main__":
 
         in_msg_tensor = torch.cat([
             torch.tensor([BOS_IDX], dtype=torch.long),
-            tensor_builder.text_to_tensor(agent1_msg, REVERSE_ENCODER_INPUTS),
+            TextTensorBuilder.text_to_tensor(all_vocab, agent1_msg, REVERSE_ENCODER_INPUTS),
             torch.tensor([EOS_IDX], dtype=torch.long)
         ], dim=-1)
-        in_doc_tensor = torch.cat([
+        in_md_tensor = torch.cat([
             torch.tensor([BMD_IDX], dtype=torch.long),
-            tensor_builder.text_to_tensor(agent1_doc, REVERSE_ENCODER_INPUTS),
+            TextTensorBuilder.text_to_tensor(all_vocab, agent1_doc, REVERSE_ENCODER_INPUTS),
             torch.tensor([EMD_IDX], dtype=torch.long)
         ], dim=-1)
 
-        if REVERSE_ENCODER_INPUTS:
-            input_seq = torch.cat([in_doc_tensor, in_msg_tensor], dim=-1)
-        else:
-            input_seq = torch.cat([in_msg_tensor, in_doc_tensor], dim=-1)
-
-        output_seq = torch.cat([
+        out_msg_tensor = torch.cat([
             torch.tensor([BOS_IDX], dtype=torch.long),
-            tensor_builder.text_to_tensor(agent2_msg),
+            TextTensorBuilder.text_to_tensor(all_vocab, agent2_msg),
             torch.tensor([EOS_IDX], dtype=torch.long)
         ], dim=-1)
 
-        tensor_data.append((input_seq, output_seq))
+        tensor_data.append((in_msg_tensor, in_md_tensor, out_msg_tensor))
 
     # Create train/validation splits, save, and exit.
-  #  tensor_data = itertools.batched(tensor_data, 20000)
- #   for i, batch in enumerate(tensor_data):
-
     train_end_idx = math.floor(len(tensor_data) * DATASET_SPLIT_RATIO) 
 
     train_iter = DataLoader(
@@ -239,9 +284,7 @@ if __name__ == "__main__":
         shuffle = SHUFFLE_DATALOADERS, collate_fn = collate,
         drop_last = DROP_LAST_BATCH)
 
-    torch.save(train_iter, "./model_tensors/train_tensor_clean2_{0}_{1}_{2}.pt".format(
-        SHUFFLE_DATALOADERS, MODEL_BATCH_SIZE, MAX_DATA))
-    torch.save(valid_iter, "./model_tensors/valid_tensor_clean2_{0}_{1}_{2}.pt".format(
-        SHUFFLE_DATALOADERS, MODEL_BATCH_SIZE, MAX_DATA))
+    torch.save(train_iter, TRAIN_OUTPUT_PATH)
+    torch.save(valid_iter, VALID_OUTPUT_PATH)
         
     exit(constants.EXIT_SUCCESS)
